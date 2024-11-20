5a568a20e7d7e21c1fdd0603d8df3b54d421386a
Parth Gangar <parthgangar97@gmail.com>
2024-11-20 18:40:50 +0530

    Delete scripts directory. Not required

--- scripts/changeKernel.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-kexec -l /boot/vmlinuz-$1 --initrd=/boot/initrd.img-$1 --append="root=UUID=c9600977-5580-47a2-872b-155e747507ca ro quiet splash amd_iommu=off numa=on vt.handoff=7"
-kexec -e
